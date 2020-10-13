0c35c51583de51c4f9b7e0e04206b5ecf106b6ea
Higor <omegadosgames02@gmail.com>
2020-10-13 17:09:02 -0300

    Criação do menu editar, nele contém: Copiar, colar, recortar e apagar.

--- main.py
+++ main.py
@@ -26,18 +26,28 @@ class Notepad:
         self.filemenu.add_separator()
         self.filemenu.add_command(label='Salvar como', command=self.save_file)
         self.filemenu.add_separator()
-        self.filemenu.add_command(label='Limpar...', command=self.clear)
         self.filemenu.add_command(label='Sair', command=self.quit_file)
 
-        # self.b1 = Button(self.root, text='save file', command=self.save_file)
-        # self.b1.place(x=10, y=10)
-        # self.b2 = Button(self.root, text='clear', command=self.clear)
-        # self.b2.place(x=70, y=10)
-        # self.b3 = Button(self.root, text='open file', command=self.open_file)
-        # self.b3.place(x=120, y=10)
+        self.editmenu = Menu(self.menubar)
+        self.menubar.add_cascade(label='Editar', menu=self.editmenu)
+        self.editmenu.add_command(label='Cortar', command=self.cut, accelerator="Ctrl+R")
+        self.editmenu.add_command(label='Copiar', command=self.copy, accelerator="Ctrl+C")
+        self.editmenu.add_command(label='Colar', command=self.paste, accelerator="Ctrl+V")
+        self.editmenu.add_separator()
+        self.editmenu.add_command(label='Limpar', command=self.clear)
+
+
         self.entry = Text(self.root, height=33, width=58, wrap=WORD)
         self.entry.place(x=10, y=50)
 
+
+    def cut(self):
+        self.entry.event_generate("<<Cut>>")
+    def paste(self):
+        self.entry.event_generate("<<Paste>>")
+    def copy(self):
+        self.entry.event_generate("<<Copy>>")
+    
     # Save file
     def save_file(self):
         # asks if you want to save the location
