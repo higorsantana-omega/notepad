c26960a024009a9d5b54600f6b64d6c7aa4da1b9
Higor <omegadosgames02@gmail.com>
2020-10-13 16:50:02 -0300

    Criação de uma menubar
    adicionado a menubar os seguintes itens: Abrir; Salvar; Limpar; Sair

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ class Notepad:
 
     def __init__(self, master=None):
         self.root = root
+        self.menubar = Menu(self.root)
+        root.config(menu=self.menubar)
         self.window()
         self.button()
         root.mainloop()
@@ -18,12 +20,21 @@ class Notepad:
         self.root.title('Notepad')
 
     def button(self):
-        self.b1 = Button(self.root, text='save file', command=self.save_file)
-        self.b1.place(x=10, y=10)
-        self.b2 = Button(self.root, text='clear', command=self.clear)
-        self.b2.place(x=70, y=10)
-        self.b3 = Button(self.root, text='open file', command=self.open_file)
-        self.b3.place(x=120, y=10)
+        self.filemenu = Menu(self.menubar)
+        self.menubar.add_cascade(label='Arquivo', menu=self.filemenu)
+        self.filemenu.add_command(label='Abrir...', command=self.open_file)
+        self.filemenu.add_separator()
+        self.filemenu.add_command(label='Salvar como', command=self.save_file)
+        self.filemenu.add_separator()
+        self.filemenu.add_command(label='Limpar...', command=self.clear)
+        self.filemenu.add_command(label='Sair', command=self.quit_file)
+
+        # self.b1 = Button(self.root, text='save file', command=self.save_file)
+        # self.b1.place(x=10, y=10)
+        # self.b2 = Button(self.root, text='clear', command=self.clear)
+        # self.b2.place(x=70, y=10)
+        # self.b3 = Button(self.root, text='open file', command=self.open_file)
+        # self.b3.place(x=120, y=10)
         self.entry = Text(self.root, height=33, width=58, wrap=WORD)
         self.entry.place(x=10, y=50)
 
@@ -51,5 +62,8 @@ class Notepad:
         if self.file is not None:
             self.content= file.read()
         self.entry.insert(INSERT, content)
+
+    def quit_file(self):
+        self.root.destroy()
 
 Notepad(root)
